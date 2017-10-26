d0d7786c9ce9bd3fe19741b1853e710c30980df7
Edward_Teach <cmpunk.ig@gmail.com>
2017-10-26 12:00:42 +0300

    Penis

--- app/src/main/java/com/example/igor/wifimanager2/MainActivity.java
+++ app/src/main/java/com/example/igor/wifimanager2/MainActivity.java
@@ -1,40 +1,56 @@
 package com.example.igor.wifimanager2;
 
 import android.content.Context;
+import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> Networkssid = new ArrayList<>();
-    ArrayList<String> networkPass = new ArrayList<>();
-    Context context;
+    String networkSSID = "Home_wifi_1";
+    String networkPass = "09820639";
+
     Button Connect_btn;
     Button Disconnect_btn;
-    TextView SSID_txt,PASS_txt;
-    WifiInfo connectionInfo;
+    public TextView con_txt,txtview;
+
+    boolean tryConnect;
+
+    WifiManager wifiManager;
+    SupplicantState connectionInfo;
     int netId;
-    int i=0;
+
+    MyTask mt;
+
+    List<WifiConfiguration> WifiConfig;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Connect_btn = (Button) findViewById(R.id.connect_btn);
-
+        con_txt = (TextView) findViewById(R.id.connectionInfo);
+        txtview= (TextView) findViewById(R.id.textView2);
         Disconnect_btn = (Button) findViewById(R.id.Disconnect);
-        SSID_txt = (TextView) findViewById(R.id.SSID);
-        PASS_txt = (TextView) findViewById(R.id.Pass);
+
+        wifiManager = (WifiManager)MainActivity.this.getSystemService(WIFI_SERVICE);
+
         Disconnect_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,59 +61,63 @@ public class MainActivity extends AppCompatActivity {
         Connect_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SetWifiInfo();
+                ConnectToWifi();
             }
         });
-        Networkssid.add("Arounda");
-        networkPass.add("27101996");
 
-        Networkssid.add("Arounda 5GHz");
-        networkPass.add("27101996");
-
-        Networkssid.add("DIR-300_15/1");
-        networkPass.add("13198155");
-
-        Networkssid.add("ATIS2");
-        networkPass.add("atis1996");
-        Networkssid.add("Salon2");
-        networkPass.add("22222222");
-        Networkssid.add("Riposino");
-        networkPass.add("09888877788");
-
-        SSID_txt.setText(Networkssid.get(0));
-        PASS_txt.setText(networkPass.get(0));
-
-
+        Timer myTimer = new Timer();
+        myTimer.schedule(new TimerTask() { // Определяем задачу
+            @Override
+            public void run() {
+                mt = new MyTask();
+                mt.execute();
+            };
+        }, 0L, 2L * 1000);
     }
 
-    public void SetWifiInfo(){
-
+    public void ConnectToWifi(){
+        wifiManager.removeNetwork(netId);
+        tryConnect = true;
         WifiConfiguration wifiConfig = new WifiConfiguration();
-        wifiConfig.SSID = String.format("\"%s\"", Networkssid.get(i));
-        wifiConfig.preSharedKey = String.format("\"%s\"", networkPass.get(i));
-        SSID_txt.setText(Networkssid.get(i));
-        PASS_txt.setText(networkPass.get(i));
-
-        WifiManager wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
+        wifiConfig.SSID = String.format("\"%s\"", networkSSID);
+        wifiConfig.preSharedKey = String.format("\"%s\"", networkPass);
         netId = wifiManager.addNetwork(wifiConfig);
         wifiManager.disconnect();
         wifiManager.enableNetwork(netId, true);
         wifiManager.reconnect();
-        connectionInfo = wifiManager.getConnectionInfo();
-        if(connectionInfo==null){
 
-            wifiManager.disconnect();
+    }
+
+    public String WiFiConnectionInfo(){
+        connectionInfo = wifiManager.getConnectionInfo().getSupplicantState();
+        if(connectionInfo.toString() == "COMPLETED")
+            tryConnect=false;
+        if(connectionInfo.toString() == "SCANNING" && !tryConnect)
             wifiManager.removeNetwork(netId);
-            i++;
-        }
+        return connectionInfo.toString();
+
     }
 
     public void Disconnect(){
-        WifiManager wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
         wifiManager.disconnect();
         wifiManager.removeNetwork(netId);
-        i++;
-        SSID_txt.setText(Networkssid.get(i));
-        PASS_txt.setText(networkPass.get(i));
+    }
+
+    class MyTask extends AsyncTask<Void, Void, String> {
+
+
+        @Override
+        protected String doInBackground(Void... noargs) {
+            return WiFiConnectionInfo();
+        }
+
+        protected void  onPostExecute(String result) {
+            con_txt.setText(result);
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    result, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
     }
 }
